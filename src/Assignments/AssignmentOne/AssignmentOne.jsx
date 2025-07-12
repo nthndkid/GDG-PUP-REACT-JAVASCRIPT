2a212114271c413f887d7d5401d464fcd9e584aa
nthndkid <143003322+nthndkid@users.noreply.github.com>
2025-07-12 13:09:17 +0800

    react-1 done

--- src/Assignments/AssignmentOne/AssignmentOne.jsx
+++ src/Assignments/AssignmentOne/AssignmentOne.jsx
@@ -5,7 +5,11 @@ function AssignmentOne() {
   const [count, setCount] = useState(0);
 
   const incrementCounter = () => setCount(count + 1);
-  const decrementCounter = () => setCount(count - 1);
+  const decrementCounter = () => {
+    if(count > 0){
+      setCount(count -1)
+    }
+  };
 
   return (
     <>
